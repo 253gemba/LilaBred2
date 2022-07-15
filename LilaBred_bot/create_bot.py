b5a30ecf69cdf694b0e596f83ce72bdd3ff2b713
Charlotte000 <ramil.mifteev@yandex.ru>
2022-07-15 21:49:14 +0300

    add pickle storage

--- LilaBred_bot/create_bot.py
+++ LilaBred_bot/create_bot.py
@@ -1,8 +1,8 @@
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.files import PickleStorage
 
 from settings import API_KEY
 
-storage = MemoryStorage()
+storage = PickleStorage('./data.pickle')
 lilabred_bot = Bot(token=API_KEY)
 dp = Dispatcher(bot=lilabred_bot, storage=storage)
