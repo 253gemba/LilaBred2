1e78b58a0bbafe5770b6192fe23494bcc8493343
Charlotte000 <ramil.mifteev@yandex.ru>
2022-07-19 03:03:58 +0300

    fix

--- js/circles.js
+++ js/circles.js
@@ -2,30 +2,33 @@ const buttons = document.querySelectorAll('#services .services__wrapper-icons .i
 const buttons_hidden = document.querySelectorAll('#services .services__wrapper-icons .icon_hidden');
 const circles = document.querySelectorAll('#services .services__wrapper-icons .circle')
 
-function reset() {
-    for (let j = 0; j < 4; j++) {
-        buttons[j].style.display = 'inline-block';
-        buttons_hidden[j].style.display = 'none;'
-        circles[j].style.display = 'none';
-    }
-}
-
 for (let i = 0; i < 4; i++) {
-    buttons[i].addEventListener('click', function () {
-        reset();
+    buttons[i].addEventListener('click', function () {        
         let index = i;
-        buttons[index].style.display = 'none';
-        buttons_hidden[index].style.display = 'inline-block';
-        circles[index].style.display = ''; 
+
+        for (let j = 0; j < 4; j++) {
+            if (j == index) {
+                buttons[j].style.display = 'none';
+                buttons_hidden[j].style.display = 'inline-block';
+                circles[j].style.display = '';
+            }
+            else {
+                buttons[j].style.display = 'inline-block';
+                buttons_hidden[j].style.display = 'none';
+                circles[j].style.display = 'none';
+            }
+        }
     });
 }
 
 for (let i = 0; i < 4; i++) {
     buttons_hidden[i].addEventListener('click', function () {
-        reset();
         let index = i;
-        buttons[index].style.display = 'inline-block';
-        buttons_hidden[index].style.display = 'none';
-        circles[index].style.display = 'none'; 
+        
+        for (let j = 0; j < 4; j++) {
+            buttons[j].style.display = 'inline-block';
+            buttons_hidden[j].style.display = 'none';
+            circles[j].style.display = 'none';
+        }
     });
 }
