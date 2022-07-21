138c955b96e1dac93fdfed72860f45fe32f7f6a4
Alexander <pupsentiy@gmail.com>
2022-07-21 15:25:21 +0300

    c

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,37 @@
 //animations circle
-const buttons = document.querySelectorAll(
-  "#services .services__wrapper-icons .icon"
-);
-const buttons_hidden = document.querySelectorAll(
-  "#services .services__wrapper-icons .icon_hidden"
-);
-const circles = document.querySelectorAll(
-  "#services .services__wrapper-icons .circle"
-);
+
+
+const buttons = document.querySelectorAll('#services .services__wrapper-icons .icon');
+const buttons_hidden = document.querySelectorAll('#services .services__wrapper-icons .icon_hidden');
+const circles = document.querySelectorAll('#services .services__wrapper-icons .circle')
 
 for (let i = 0; i < 4; i++) {
-  buttons[i].addEventListener("click", function () {
-    let index = i;
-
-    for (let j = 0; j < 4; j++) {
-      if (j == index) {
-        buttons[j].style.display = "none";
-        buttons_hidden[j].style.display = "inline-block";
-        circles[j].style.display = "";
-      } else {
-        buttons[j].style.display = "inline-block";
-        buttons_hidden[j].style.display = "none";
-        circles[j].style.display = "none";
-      }
-    }
-  });
+    buttons[i].addEventListener('click', function () {        
+        let index = i;
+        for (let j = 0; j < 4; j++) {
+            if (j == index) {
+                buttons[j].style.display = 'none';
+                buttons_hidden[j].style.display = 'inline-block';
+                circles[j].style.display = '';
+            }
+            else {
+              circles[j].style.display = 'none';
+              buttons_hidden[j].style.display = 'block  ';
+                buttons[j].style.display = 'inline-block';
+            }
+        }
+    });
 }
 
 for (let i = 0; i < 4; i++) {
-  buttons_hidden[i].addEventListener("click", function () {
-    let index = i;
-
-    for (let j = 0; j < 4; j++) {
-      buttons[j].style.display = "inline-block";
-      buttons_hidden[j].style.display = "none";
-      circles[j].style.display = "none";
-    }
-  });
+  circles[i].addEventListener('click', function () {
+        let index = i;
+        for (let j = 0; j < 4; j++) {
+          buttons[j].style.display = 'inline-block';
+          buttons_hidden[j].style.display = 'block';
+          circles[j].style.display = 'none';
+      }
+    });
 }
 
 ///бургер меню
@@ -216,10 +211,4 @@ $(document).ready(function () {
   });
 });
 
-// grid table
-const elem = document.querySelector(".grid");
-const iso = new Isotope(elem, {
-  itemSelector: ".grid-item",
-  layoutMode: "masonry",
-  masonry: {},
-});
+
